e6ee5e15e56ba1c3c21d9e748f038e9783baf01b
German Gonzalez <german.glz01@gmail.com>
2021-07-06 13:53:47 -0500

    main - reusing components(12).

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'Uno', body: 'lorem ipsum...', author: 'Mario', id: 1 },
         { title: 'dos', body: 'lorem ipsum...', author: 'Galuz', id: 2 },
-        { title: 'tres', body: 'lorem ipsum...', author: 'Daniela', id: 3 },
+        { title: 'tres', body: 'lorem ipsum...', author: 'Mario', id: 3 },
     ])
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title='All blogs'/>
+            <BlogList blogs={blogs.filter((blogs) => blogs.author === 'Mario')} title="Mario's blogs"/>
         </div>
     );
 }
