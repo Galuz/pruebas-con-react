b3f49b757451194087b9de187e7583ae8ce8f708
German Gonzalez <german.glz01@gmail.com>
2021-07-07 17:19:51 -0500

    main - fetching data(17)

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,24 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'Uno', body: 'lorem ipsum...', author: 'Mario', id: 1 },
-        { title: 'dos', body: 'lorem ipsum...', author: 'Galuz', id: 2 },
-        { title: 'tres', body: 'lorem ipsum...', author: 'Mario', id: 3 },
-    ])
+    const [blogs, setBlogs] = useState(null)
 
     const [name, setName] = useState('Mario')
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
-
-    useEffect(() => { // use efect se ejecuta cada que el sitio de rederiza.
-        console.log('use effect ran')
-        console.log(blogs)
-        console.log(name, 'name')
-    }, [name]) // si usas el segundo argumento vacio, solo se ejecutara la primera vez que la pagina se cargue, pero si agregas un valor se ejecutara cada que ese valor cambie. 
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(response => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            setBlogs(data)
+        })
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete}/>
-            <button onClick={() => setName('luigi')}>Change name</button>
-            <p>{name}</p>
+           {blogs && <BlogList blogs={blogs} title='All blogs'/> }
         </div>
     );
 }
