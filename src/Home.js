f589491d403be74613fe30cd8c1820da5ad8cb4f
German Gonzalez <german.glz01@gmail.com>
2021-07-09 15:32:55 -0500

    main - custom hook.

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,9 @@
 import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(response => {
-                console.log(response)
-                if(!response.ok){
-                    throw Error('could not fetch the data fot that resource')
-                }
-                return response.json()
-            })
-            .then((data) => {
-                setBlogs(data)
-                setIsPending(false)
-                setError(null)
-            })
-            .catch((e) => {
-                setIsPending(false)
-                setError(e.message)
-            })
-        }, 1000)
-    }, [])
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
     return (
         <div className="home">
---
+++ src/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null) 
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+      .then(response => {
+          console.log(response)
+          if(!response.ok){
+              throw Error('could not fetch the data fot that resource')
+          }
+          return response.json()
+      })
+      .then((data) => {
+          setData(data)
+          setIsPending(false)
+          setError(null)
+      })
+      .catch((e) => {
+          setIsPending(false)
+          setError(e.message)
+      })
+    }, 1000)
+  }, [url])
+  
+  return { data, isPending, error }
+}
+
+export default useFetch
